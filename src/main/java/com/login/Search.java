cb87aff146dad16d0a2976f8b6de9aae75725a0b
Devansh Dwivedee <dwivedee.devansh@gmal.com>
2022-03-17 17:09:03 +0530

    added new files

--- src/main/java/com/login/Search.java
+++ src/main/java/com/login/Search.java
@@ -97,7 +97,10 @@ public class Search extends HttpServlet {
 
                      out.print("<td>"+rs1.getString(4)+"</td></tr>");    
                      
-                     
+                     int x=Integer.parseInt(rs1.getString(3))*12;
+                     int y=Integer.parseInt(rs1.getString(4));
+                     int z=x+y;
+                     out.print("<td>"+" Annual Salary is : "+ z +"</td></tr>");    
                      out.print("<td> </td>");
                   }
 
